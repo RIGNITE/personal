c8b73a8d117260d4bec5bbdb2278d7b40bc9dc0f
Ryan Kim <77694496+RIGNITE@users.noreply.github.com>
2023-08-07 10:31:46 -0400

    bug fix - about me page

--- components/list-items.vue
+++ components/list-items.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <div class="list__items-container"
-            v-for="(item, index) in list" 
+            v-for="(item, index) in props.list"
             :key="index"
         >
             <div class="flex row py-4 px-2">
-                <img :src="item.logo" :width="logoWidth" class="mr-3"/>
+                <img :src="item?.logo" :width="props.logoWidth" class="mr-3"/>
                 <div class="flex flex-col">
                     <span class="text-2xl">{{item.name}}</span>
                     <span>{{item.title}}</span>
@@ -16,28 +16,18 @@
     </div>
 </template>
 
-<script>
-export default {
-    name: 'ListItems',
-    props: {
-        list: {
-            type: Array,
-            required: true
-        },
-        logoWidth: {
-            type: String,
-            required: false
-        }
-    },
+<script setup>
+const props = defineProps({
+    list: Array,
+    logoWidth: String,
 }
+)
 </script>
 
 <style>
-
 .list__items-container {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
 }
-
 </style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,35 +1,24 @@
 <template>
-    
     <div class="about-container max-w-4xl mx-auto">
-        <div 
-            v-for="about in about_me_data" 
-            :key="about.title" 
-            class="about-container__card w-full"
-        >
-            <h1 class="about-title text-4xl p-2">{{about.title}}</h1>
-            <div class="list-items close">
-                <ListItems :list="about.data" :logoWidth="about.logoWidth"/>
-            </div>
-        </div>
-    </div>
-
-    <!-- <div class="about-container max-w-4xl mx-auto">
-        <div 
-            v-for="data in about_me_data" 
+        <div
+            v-for="(data) in about_me_data"
             :key="data.title"
-            @click="toggleList(data.open)"
+            @click="toggleList(data)"
             class="about-container__card w-full"
         >
             <h1 class="about-title text-4xl p-2">{{data.title}}</h1>
+            <!-- <div class="list-items" :class="isOpen ? '' : 'close'"> -->
                 <div class="list-items" :class="data.open ? 'close' : ''">
                 <ListItems :list="data.data" :logoWidth="data.logoWidth"/>
             </div>
         </div>
-    </div> -->
+    </div>
 </template>
 
 <script setup>
-const about_me_data = reactive([
+import {ref} from 'vue';
+import ListItems from './ListItems.vue';  
+const about_me_data = ref([
     {
         title: "Experience",
         open: false,
@@ -40,13 +29,13 @@ const about_me_data = reactive([
                 year: "May 2023 - August 2023",
                 title: "Frontend Developer Intern",
                 logo: "/diff.jpg"
-            }, 
+            },
             {
                 name: "Ubisoft",
                 year: "May 2022 - August 2022",
                 title: "CRM Frontend Developer Intern",
                 logo: "/ubilogo.png"
-            }, 
+            },
             {
                 name: "HRWize",
                 year: "May 2021 - August 2021",
@@ -89,28 +78,9 @@ const about_me_data = reactive([
     }
 ])
 
-
-// const toggleList = (open) => {
-//     console.log(open);
-//     open.value = !open.value;
-//     // console.log('hello');
-// }
-
-onMounted(() => {
-    const aboutCards = document.querySelectorAll('.about-container__card');
-    // console.log(aboutCards);
-    aboutCards.forEach(card => {
-        const listItems = card.querySelector('.list-items');
-        card.addEventListener('click', () => {
-            listItems.classList.toggle('close');
-        })
-        // Keep black space open once user clicks header
-
-        // card.addEventListener('mouseleave', () => {
-        //     listItems.classList.add('close');
-        // })
-    })
-})
+const toggleList = (data) => {
+    data.open = !data.open;
+}
 
 </script>
 
@@ -122,27 +92,22 @@ onMounted(() => {
     align-items: flex-start;
     padding: 40px;
 }
-
 .about-container__card {
     max-width: 800px;
 }
-
 .about-title{
     cursor: pointer;
-    transition: all 0.3s ease; /* Adjust the duration and easing as needed */    
+    transition: all 0.3s ease; /* Adjust the duration and easing as needed */
     background: linear-gradient(to right,black 50%,  white 50% ) right;
     background-size: 200% 100%;
 }
-
 .about-title:hover{
     letter-spacing: 0.1em;
     background-position: left;
     transition:all 0.8s ease;
     color: white;
 }
-
 .close{
     display: none;
 }
-
 </style>
