d5b0aafc6c7274bfae6136a6b423627070be1957
Ryan Kim <rck2001@hotmail.com>
2023-08-15 15:18:38 -0400

    WIP using GSAP for progress bar using Scroll Trigger

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,28 +1,78 @@
 
 <template>
-  <div class="project__container">
-    <h1 class="text-4xl">{{ project.primary.name }}</h1>
-    <p><PrismicRichText :field="project.primary.blurp" /></p>
+  <div>
+    <progress max="100" value="0" ref="progress_bar"></progress>
+    <div class="project__container">
+      <h1 class="text-4xl tracking-widest">{{ project.primary.name }}</h1>
+      <p>{{ project.primary.date }}</p>
+      <p>{{ project.primary.reason }} - {{ project.primary.team }}</p>
+      <p class="py-4"><PrismicRichText :field="project.primary.blurp" /></p>
+      <div class="a" ref="box">a</div>
+      <p>{{ project.primary.skills }}</p>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const box = ref(null);
+const progress_bar = ref(null);
 const route = useRoute();
-
-if (!route.params.slug) {
-  console.log('No ID provided')
-}
-
+// Fetch the project data from Prismic
 const prismic = usePrismic();
 const { data: page } = await useAsyncData("[home]", () =>
   prismic.client.getSingle("projects")
 );
+// Find the project that matches the slug
+const project = page?.value?.data?.slices?.find(
+  (slice) => slice.primary?.slug === route.params?.slug
+);
 
-const project = page?.value?.data?.slices?.find((slice) => slice.primary.slug === route.params.slug);
-console.log(project);
+// Routes used to get the slug from the URL
+if (!route.params.slug) {
+  console.log('No ID provided')
+}
+
+// ScrollTrigger Animations
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(() => {
+  gsap.to(box.value, {
+    scrollTrigger: {
+      trigger: box.value,
+      toggleActions: "restart pause none none",
+    },
+    x: 500,
+    rotation: 360,
+    duration: 3,
+  });
+
+  gsap.to(progress_bar.value, {
+    value: 100,
+    ease: none,
+    scrollTrigger: {
+      start: 0,
+      end: 100,
+      scrub: 0.3,
+    }
+  });
+});
 </script>
 
+
 <style>
+progress {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 5px;
+  border: none;
+  background: transparent;
+}
+
 .project__container {
   display: flex;
   flex-direction: column;
@@ -31,5 +81,15 @@ console.log(project);
   max-width: 48rem;
   margin-left: auto;
   margin-right: auto;
+}
+
+.a {
+  display: flex;
+  padding: 30px;
+  background-color: red;
+  width: 40px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
 }
 </style>
