99610ce30d65786ba736790852bfc26fd2842121
Ryan Kim <77694496+RIGNITE@users.noreply.github.com>
2023-08-21 14:55:50 -0400

    Mobile responsive and less repetitive css

--- components/list-items.vue
+++ components/list-items.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="py-3">
-        <div v-if="props.category === 'Passions'" class="flex row py-4 px-2">
+    <div class="pb-3">
+        <div v-if="props.category === 'Passions'" class="list__items-container">
             <div v-for="(item, index) in props.list"
                 :key="props.category + index"
             >
-                <h1 class="text-2xl font-medium p-2">{{ item.clubs}}</h1>
-                <div class="flex row">
-                    <p class="max-w-sm px-2">{{ item.description }}</p>
+                <h1 class="passion-header">{{ item.clubs }}</h1>
+                <div class="passion-container__info">
+                    <p class="max-w-sm mx-auto px-2">{{ item.description }}</p>
                     <swiper
                         class="passion-swiper"
                         :modules="[SwiperAutoplay, SwiperPagination, SwiperNavigation]"
@@ -29,10 +29,10 @@
             v-for="(item, index) in props.list"
             :key="props.category + index"
         >
-            <div class="flex row py-4 px-2">
-                <prismic-image :field="item.logo" class="mr-3"/>
+            <div class="list-item-container">
+                <prismic-image :field="item.logo" class="logo"/>
                 <div class="flex flex-col">
-                    <span class="text-2xl">{{item.name}}</span>
+                    <span class="header-text">{{item.name}}</span>
                     <span>{{item.title}}</span>
                     <span>{{item.year}}</span>
                 </div>
@@ -51,7 +51,66 @@ const props = defineProps({
 </script>
 
 <style>
+.list-item-container {
+    display: flex; 
+    flex-direction: row;
+    padding: 1rem 0.5rem;
+}
+
+.passion-container__info {
+    display: flex;
+    flex-direction: row;
+}
+
 .passion-swiper {
     max-width: 400px;
+}
+
+.passion-header {
+    padding: 0.5rem; 
+    font-size: 1.5rem;
+    line-height: 2rem; 
+    font-weight: 500;
+}
+
+.logo {
+    margin-right: 0.75rem;
+}
+
+.header-text {
+    font-size: 1.5rem;
+    line-height: 2rem; 
+}
+
+.text {
+    font-size: 1.25rem;
+    line-height: 1.75rem; 
+}
+
+@media screen and (max-width: 650px) {
+    .list-item-container {
+        display: block;
+    }
+
+    .passion-container__info {
+        display: block;
+        flex-direction: column;
+    }
+
+    .passion-swiper {
+        margin-top: 20px;
+        max-width: 24rem; 
+    }
+
+    .passion-header {
+        max-width: 24rem; 
+        margin-right: auto;
+        margin-left: auto;
+    }
+
+    .logo {
+        margin-right: 0;
+        margin-bottom: 0.75rem;
+    }
 }
 </style>
